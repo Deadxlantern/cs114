ac95e15a6ec26d1be5502cd62a0a574a2f747fbe
Student <student@ai241lab0203m12.aipdcls.edu>
2017-04-13 15:13:09 -0700

    newfile

--- no.py
+++ no.py
@@ -1,3 +1,4 @@
 print("Hi My Name is Dave. I am a a working ai in production by sun labs.")
 print("Sun labs is planning on taking over your brain.")
 print("Are you willing to submit to me human")
+print("If you do not submit you will be taken and used")
