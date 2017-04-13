b393e70656d36c8c85672723c1d2be5324c8c382
Student <student@ai241lab0203m12.aipdcls.edu>
2017-04-13 16:23:11 -0700

    jokes

---
+++ mo.py
@@ -0,0 +1,11 @@
+from time import sleep
+print("How often do you like jokes about elementals")
+sleep 3
+print("Periodically")
+from time import sleep
+print("What did the ottoman empire say when invading europe")
+sleep 2
+print("Are you hungary for some turkey")
+from time import sleep
+sleep 2
+print("there is norway you wont find this not funny")
