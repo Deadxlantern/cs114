5ca7163e53c2687b4be964f8e0562199a44e4740
Deadxlantern <druidofthenight5@gmail.com>
2017-05-04 16:49:36 -0700

    Add files via upload

---
+++ claytonmad.py
@@ -0,0 +1,24 @@
+"""MAdlib"""
+from time import sleep
+#1. Setup
+
+#2. input
+print ("Please Fill in the following Nouns, Adj, Verb adverb")
+sleep (1)
+print ("Please give me a noun")
+noun = input()
+sleep (1)
+print ("Please give me an adverb")
+adverb = input()
+sleep (1)
+print ("please give me a verb")
+verb = input()
+sleep (1)
+print (" please give me a adj")
+adj =input()
+sleep (1)
+print ("Please give me an another noun")
+noun1 = input()
+
+#4. output
+print ("The",noun,adverb,verb,adj,noun1,)
